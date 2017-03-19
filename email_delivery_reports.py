d62fcdb51fe33d1c183d7a03e8ac7d7a30eb16e7
Mathias Rav <rav@cs.au.dk>
2017-03-19 19:05:59 -0400

    Add email_delivery_reports.py

---
+++ email_delivery_reports.py
@@ -0,0 +1,10 @@
+from tkmail.delivery_reports import email_delivery_reports, dump_stats
+
+
+# prefixes = {}
+# postfixes = {}
+
+
+for errorname, report in email_delivery_reports():
+    print(errorname, report.notification)
+dump_stats()
