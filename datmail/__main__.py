81937432b435fc5f20dff853a3af45e376e48edf
Anders Bruun Severinsen <202204885@post.au.dk>
2025-10-18 10:46:40 +0200

    Update relay host to use 'host.docker.internal' in main.py and add extra_hosts configuration in docker-compose.yml

--- datmail/__main__.py
+++ datmail/__main__.py
@@ -6,11 +6,11 @@ from datmail.server import DatForwarder
 
 
 def configure_logging():
-    file_handler = logging.FileHandler('datmail.log', 'a')
+    file_handler = logging.FileHandler("datmail.log", "a")
     stream_handler = logging.StreamHandler(None)
-    fmt = '[%(asctime)s %(levelname)s] %(message)s'
+    fmt = "[%(asctime)s %(levelname)s] %(message)s"
     datefmt = None
-    formatter = logging.Formatter(fmt, datefmt, '%')
+    formatter = logging.Formatter(fmt, datefmt, "%")
     for handler in (file_handler, stream_handler):
         handler.setFormatter(formatter)
         logger.addHandler(handler)
@@ -18,29 +18,26 @@ def configure_logging():
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--port', type=int, default=25,
-                    help='Relay port')
-parser.add_argument('-P', '--listen-port', type=int, default=9000,
-                    help='Listen port')
+parser.add_argument("-p", "--port", type=int, default=25, help="Relay port")
+parser.add_argument("-P", "--listen-port", type=int, default=9000, help="Listen port")
 
 
 def main():
     configure_logging()
     args = parser.parse_args()
 
-    receiver_host = '0.0.0.0'
+    receiver_host = "0.0.0.0"
     receiver_port = args.listen_port
-    relay_host = '127.0.0.1'
+    relay_host = "host.docker.internal"
     relay_port = args.port
 
-    server = DatForwarder(
-        receiver_host, receiver_port, relay_host, relay_port)
+    server = DatForwarder(receiver_host, receiver_port, relay_host, relay_port)
     try:
         server.run()
     except Exception as exn:
-        logger.exception('Uncaught exception in DatForwarder.run')
+        logger.exception("Uncaught exception in DatForwarder.run")
     else:
-        logger.info('DatForwarder exiting')
+        logger.info("DatForwarder exiting")
 
 
 if __name__ == "__main__":
