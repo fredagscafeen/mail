516d5055d83e4ebe059cf4ffefe85f9445b6fb31
Anders Bruun Severinsen <202204885@post.au.dk>
2025-10-18 10:18:16 +0200

    Add port parameter to database connection and refactor docker-compose to use host network mode

--- datmail/database.py
+++ datmail/database.py
@@ -7,7 +7,11 @@ from datmail.config import HOSTNAME, USERNAME, PASSWORD, DATABASE
 class Database(object):
     def __init__(self):
         self._conn = psycopg2.connect(
-            host=HOSTNAME, database=DATABASE, user=USERNAME, password=PASSWORD
+            host=HOSTNAME,
+            database=DATABASE,
+            user=USERNAME,
+            password=PASSWORD,
+            port=5432,
         )
         self._cursor = self._conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
